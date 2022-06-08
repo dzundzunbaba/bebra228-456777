4ca17eb68776764b23f49873ece632272140e069
dzundzunbaba <103132561+dzundzunbaba@users.noreply.github.com>
2022-06-08 16:23:56 +0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,51 +1,69 @@
-from pygame import *
-
-back = (255, 182, 193)
-win_width = 600
-win_height = 500
-window = display.set_mode((win_width, win_height))
-window.fill(back)
-
-game = True
-finish = False
-clock = time.Clock()
-FPS = 60
-
-
-class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-       sprite.Sprite.__init__(self)
-       self.image = transform.scale(image.load(player_image), (size_x, size_y))
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.x > 5:
-           self.rect.x -= self.speed
-       if keys[K_DOWN] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
-class Player(GameSprite):
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.x > 5:
-           self.rect.x -= self.speed
-       if keys[K_s] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
-
-racket1 = Player('darth-vader-clip-art-4.png', 30, 200, 30, 300,150)
-racket2 = Player('darth-vader-clip-art-4.png', 30, 200, 30, 300,150)
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    if finish != True:
-        racket1.reset()
-        racket2.reset()
-
-    display.update()
-    clock.tick(FPS)
+<html>
+    <title>sw</title>
+    <h1>Звездные воины</h1>
+     <table style = "border: 1px solid blue">
+        <tr>
+            <td style = "border: 1px solid blue">фильм</td>
+            <td style = "border: 1px solid blue">год</td>
+            <td style = "border: 1px solid blue">рейтинг</td>
+            
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: Новая надежда (1977)</td>
+            <td style = "border: 1px solid blue">(1977)</td>
+            <td style = "border: 1px solid blue">11</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: эпизод V - империя наносит ответный удар </td>
+            <td style = "border: 1px solid blue">(1980)</td>
+            <td style = "border: 1px solid blue">10</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: Эпизод VI - возвращение джедая </td>
+            <td style = "border: 1px solid blue">(1983)</td>
+            <td style = "border: 1px solid blue">9</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Изгой-один: звёздные войны. Истории </td>
+            <td style = "border: 1px solid blue">(2016)</td>
+            <td style = "border: 1px solid blue">8</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: пробуждение силы </td>
+            <td style = "border: 1px solid blue">(2015)</td>
+            <td style = "border: 1px solid blue">7</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: эпизод III - месть ситхов </td>
+            <td style = "border: 1px solid blue">(2005)</td>
+            <td style = "border: 1px solid blue">6</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: эпизод I - скрытая угроза </td>
+            <td style = "border: 1px solid blue">(1999)</td>
+            <td style = "border: 1px solid blue">5</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: эпизод II - атака клонов</td>
+            <td style = "border: 1px solid blue"> (2002)</td>
+            <td style = "border: 1px solid blue">4</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звёздные войны: последние джедаи</td>
+            <td style = "border: 1px solid blue">2017</td>
+            <td style = "border: 1px solid blue">3</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Хан Соло: звёздные войны. Истории
+</td>
+            <td style = "border: 1px solid blue">(2018)</td>
+            <td style = "border: 1px solid blue">2</td>
+        </tr>
+        <tr>
+            <td style = "border: 1px solid blue">Звездные войны Скайвокер восход.
+</td>
+            <td style = "border: 1px solid blue">(2019)</td>
+            <td style = "border: 1px solid blue">0</td>
+        </tr>
+    </table>
+</html>
